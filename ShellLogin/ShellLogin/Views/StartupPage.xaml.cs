a618b3938b8e6c5c25121ada18b51d827ff683f7
Damir Arh <damir.arh@gmail.com>
2020-10-18 13:28:12 +0200

    StartupPage to check for valid login
    Go into app or open LoginPage based on the result

---
+++ ShellLogin/ShellLogin/Views/StartupPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using ShellLogin.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShellLogin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartupPage : ContentPage
+    {
+        public StartupPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            CheckLogin();
+        }
+
+        private async Task CheckLogin()
+        {
+            // should check for valid login instead
+            await Task.Delay(2000);
+
+            // only open Login page when no valid login
+            await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+        }
+    }
+}
