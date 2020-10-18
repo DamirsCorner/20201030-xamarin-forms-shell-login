59af6f1e1a029da9f1168d631d1854c836ae866e
Damir Arh <damir.arh@gmail.com>
2020-10-18 12:16:10 +0200

    Add project files.

---
+++ ShellLogin/ShellLogin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using ShellLogin.ViewModels;
+
+namespace ShellLogin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ ShellLogin/ShellLogin/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using ShellLogin.Models;
+using ShellLogin.ViewModels;
+
+namespace ShellLogin.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
